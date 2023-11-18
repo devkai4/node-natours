be08771b5ef82901ae4e05632d507763a41476ed
devkai <irakoseimei4@gmail.com>
2023-11-18 10:09:30 +0900

    Environment Variables

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
-
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware👋');
-    next();
+  console.log('Hello from the middleware 👋');
+  next();
 });
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // 3) ROUTES
