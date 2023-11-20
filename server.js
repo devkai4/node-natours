05b3c3bfc65d061f14b7fa9769b3950b557c4181
devkai <irakoseimei4@gmail.com>
2023-11-20 21:02:39 +0900

    Refacttoring for MVC

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
+  process.env.DATABASE_PASSWORD,
 );
 
 mongoose
@@ -14,41 +14,9 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => console.log('DB connection successful!'));
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then(doc => {
-  console.log(doc);
-  })
-  .catch(err => {
-  console.log('ERROR💥', err);
-  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
