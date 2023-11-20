ca05233b8a8340e1ce798e5074001f0e497f55dd
devkai <irakoseimei4@gmail.com>
2023-11-20 20:20:00 +0900

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -25,15 +25,30 @@ const tourSchema = new mongoose.Schema({
     unique: true
   },
   rating: {
-    type: Number
-    default: 4.5
+    type: Number,
+    default: 4.5,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price']
-  }
+    required: [true, 'A tour must have a price'],
+  },
 });
+
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then(doc => {
+  console.log(doc);
+  })
+  .catch(err => {
+  console.log('ERROR💥', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
