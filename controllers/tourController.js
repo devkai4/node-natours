b12daae02edbaac45ca9272c520e69ef36d3fabb
devkai <irakoseimei4@gmail.com>
2023-11-25 05:40:55 +0900

    Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,21 +3,31 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
+    // {difficulty: 'easy', duration: { $ gte: 5 } }
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+    // gte, gt, lte, lt
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    console.log(req.query);
+
+    // EXECUTE QUERY
+    const tours = await query;
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
-
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
